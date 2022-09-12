d6819eb78c74ef7a194601fd5ac7b6e84d0e5e09
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-12 22:03:39 +0100

    PS: Added columns and defualt constructer

--- src/main/java/com/bae/main/entities/SerialKillerProject.java
+++ src/main/java/com/bae/main/entities/SerialKillerProject.java
@@ -1,5 +1,6 @@
 package com.bae.main.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,5 +13,24 @@ public class SerialKiller{
 	@Id 
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private long id;
+
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
+
+	@Column(nullable = false)
+	private String place;
+
+	@Column(nullable = false)
+	private int confirmedKills;
+
+	@Column(nullable = false)
+	private String zodiacSign;
+
+	public SerialKiller() {
+		super();
+	}
 }
 }
