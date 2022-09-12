68b47d3c6b6f593fb9815c9926e8100e7bf25093
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-12 22:12:59 +0100

    PS: Added create and get by id

--- src/main/java/com/bae/main/controller/SerialKillerProjectController.java
+++ src/main/java/com/bae/main/controller/SerialKillerProjectController.java
@@ -1,7 +1,15 @@
 package com.bae.main.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.main.entities.SerialKillerProject.SerialKiller;
 
 @RestController
 @RequestMapping("/SerialKiller")
@@ -10,5 +18,15 @@ public class SerialKillerProjectController {
 	public SerialKillerProjectController(SerialKillerProjectServices service) {
 		super();
 		this.serivce = service;
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<SerialKiller> create(@RequestBody SerialKiller input) {
+		return new ResponseEntity<Serialkiller>(service.create(input), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getById/{id}")
+	public SerialKiller getById(@PathVariable long id) {
+		return this.service.getById(id);
 	}
 }
