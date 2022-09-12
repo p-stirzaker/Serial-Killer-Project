2073a8f42d3a471ac47d72fadd4b93d386dc47b4
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-12 21:57:13 +0100

    PS: added schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `serial_killer`
+
+CREATE TABLE `serial_killer` (
+	`id` BIGINT AUTO_INCREMENT,
+	`first_name` VARCHAR(255) NOT NULL,
+	`last_name` VARCHAR(255) NOT NULL,
+	`place` VARCHAR(255) NOT NULL,
+	`confirmed_kills` INT NOT NULL,
+	`zodiac_sign` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+	);
