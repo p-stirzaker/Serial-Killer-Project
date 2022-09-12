9524984335b1d1c056c6248126452dbf057cd081
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-12 21:52:00 +0100

    Initialization

---
+++ src/main/java/com/bae/main/SerialKillerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerialKillerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerialKillerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/main/SerialKillerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerialKillerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
