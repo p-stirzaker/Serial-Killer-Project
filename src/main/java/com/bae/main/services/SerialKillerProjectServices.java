b0dfdfc3bffc960a180484452b94f898c87d61a1
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-12 22:22:05 +0100

    PS: Added service connection to repo

---
+++ src/main/java/com/bae/main/services/SerialKillerProjectServices.java
@@ -0,0 +1,11 @@
+package com.bae.main.services;
+
+public class SerialKillerProjectServices {
+
+	private SerialKillerProjectRepo;
+	
+	public SerialKillerProjectServices(SerialKillerProjectRepo repo) {
+		super();
+		this.repo = repo;
+	}
+}
