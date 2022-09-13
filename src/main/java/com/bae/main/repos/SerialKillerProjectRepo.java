2660cb238d082742520c7b6285314cc38938b5ca
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-13 12:14:32 +0100

    PS: Added find by serial killer first name and last name

--- src/main/java/com/bae/main/repos/SerialKillerProjectRepo.java
+++ src/main/java/com/bae/main/repos/SerialKillerProjectRepo.java
@@ -1,5 +1,7 @@
 package com.bae.main.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.bae.main.entities.SerialKillerProject.SerialKiller;
 @Repository
 public interface SerialKillerProjectRepo extends JpaRepository<SerialKiller, Long> {
 	
-
+	List<SerialKiller> findSerialKillerByFirstName(String firstName);
+	
+	List<SerialKiller> findSerialKillerByLastName(String lastName);
 }
