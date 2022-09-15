1dfee2a97042badc190641b1e6980e2b72b39d8b
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-15 16:38:36 +0100

    PS: Added changes to the project html and js files

--- src/main/resources/static/SerialKillerProject/js/main.js
+++ src/main/resources/static/SerialKillerProject/js/main.js
@@ -1,5 +1,10 @@
 "use strict";
 
+
+
+
+
+
 let getAll = () => {
     axios.get("http://localhost:8082/SerialKiller/getAll")
     .then(res => {
@@ -48,3 +53,61 @@ let del = () => {
         getAll();
     }).catch(err => console.log(err));
 }
+let search =() => {
+    let selected = document.getElementById("search-select").value
+    console.log(selected)
+
+    let input = document.getElementById("search-input").value
+    console.log(input)
+
+    axios.get(`http://localhost:8082/SerialKiller/${selected}/${input}`)
+    .then(res => {
+        console.log(res.data);
+        let cardsContainer = document.getElementById("cards")
+        let div = document.createElement("div")
+        div.classList.add("col")
+        div.innerHTML = `
+        <div class="card shadow-sm">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
+
+        <div class="card-body">
+          <p class="card-text">
+            <ul>
+                <li>
+                    First name: ${res.data.firstName}
+                </li>
+                <li>
+                    Last name: ${res.data.lastName}
+                </li>
+                <li>
+                    Place: ${res.data.place}
+                </li>
+                <li>
+                    Confirmed kills: ${res.data.confirmedKills}
+                </li>
+                <li>
+                    Zodiac sign: ${res.data.zodiacSign}
+                </li>
+            </ul>      
+        </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Update</button>
+              <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+`;
+    cardsContainer.appendChild(div)
+    }).catch(err => console.log(err));
+}
+
+let selectUpdate = () => {
+    let e = document.getElementById("search-select")
+    let text = e.options[e.selectedIndex].dataset.help
+    console.log(text)
+    document.getElementById("search-input").placeholder = text
+}
+
