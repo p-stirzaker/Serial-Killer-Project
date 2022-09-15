43c93ad3ce6bac06d3aeecdb7e7f526289e8fc5f
p.stirzaker <phoebestirzaker@gmail.com>
2022-09-15 17:46:31 +0100

    PS: Added get by functions, working delete function and create modal

--- src/main/resources/static/SerialKillerProject/js/main.js
+++ src/main/resources/static/SerialKillerProject/js/main.js
@@ -1,14 +1,54 @@
 "use strict";
 
+let createManyCards = (serialKillers) => {
+    let cardsContainer = document.getElementById("cards")
+    cardsContainer.innerHTML=""
+    serialKillers.forEach(serialKiller => {
+        let div = document.createElement("div")
+        div.classList.add("col")
+        div.innerHTML = `
+            <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
 
+            <div class="card-body">
+                <p class="card-text">
+                <ul>
+                    <li>
+                        First name: ${serialKiller.firstName}
+                    </li>
+                    <li>
+                        Last name: ${serialKiller.lastName}
+                    </li>
+                    <li>
+                        Place: ${serialKiller.place}
+                    </li>
+                    <li>
+                        Confirmed kills: ${serialKiller.confirmedKills}
+                    </li>
+                    <li>
+                        Zodiac sign: ${serialKiller.zodiacSign}
+                    </li>
+                </ul>      
+            </p>
+                <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Update</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary" data-id="${serialKiller.id}" onclick="del(this)">Delete</button>
+                </div>
 
-
-
+                </div>
+            </div>
+            </div>
+        `;
+        cardsContainer.appendChild(div)
+        });
+}
 
 let getAll = () => {
     axios.get("http://localhost:8082/SerialKiller/getAll")
     .then(res => {
         console.log(res.data);
+        createManyCards(res.data)
     }).catch(err => console.log(err));
 }
 
@@ -46,14 +86,17 @@ let update = () => {
     }).catch(err => console.log(err));
 }
 
-let del = () => {
-    axios.delete("http://localhost:8082/SerialKiller/delete/")
+let del = (serialKiller) => {
+    console.log(serialKiller);
+    axios.delete(`http://localhost:8082/SerialKiller/delete/${serialKiller.dataset.id}`)
     .then(res => {
         console.log(res.data);
         getAll();
     }).catch(err => console.log(err));
 }
+
 let search =() => {
+    document.getElementById("cards").innerHTML = ""
     let selected = document.getElementById("search-select").value
     console.log(selected)
 
@@ -62,45 +105,11 @@ let search =() => {
 
     axios.get(`http://localhost:8082/SerialKiller/${selected}/${input}`)
     .then(res => {
-        console.log(res.data);
-        let cardsContainer = document.getElementById("cards")
-        let div = document.createElement("div")
-        div.classList.add("col")
-        div.innerHTML = `
-        <div class="card shadow-sm">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
-
-        <div class="card-body">
-          <p class="card-text">
-            <ul>
-                <li>
-                    First name: ${res.data.firstName}
-                </li>
-                <li>
-                    Last name: ${res.data.lastName}
-                </li>
-                <li>
-                    Place: ${res.data.place}
-                </li>
-                <li>
-                    Confirmed kills: ${res.data.confirmedKills}
-                </li>
-                <li>
-                    Zodiac sign: ${res.data.zodiacSign}
-                </li>
-            </ul>      
-        </p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Update</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
-            </div>
-
-          </div>
-        </div>
-      </div>
-`;
-    cardsContainer.appendChild(div)
+        let data = res.data
+        if (Object.prototype.toString.call(data) !== '[object Array]') {
+            data = [data]
+        }
+        createManyCards(data)
     }).catch(err => console.log(err));
 }
 
